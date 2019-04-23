f5e2c82632a1c97eb4b37cf630445bc2d95593d0
Daniel Høyer Iversen <mail@dahoiv.net>
2019-04-23 18:47:06 +0200

    0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'PySwitchbot',
     packages = ['switchbot'],
     install_requires=['bluepy'],
-    version = '0.5',
+    version = '0.6',
     description = 'A library to communicate with Switchbot',
     author='Daniel Hoyer Iversen',
     url='https://github.com/Danielhiversen/pySwitchbot/',
