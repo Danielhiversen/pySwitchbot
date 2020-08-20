cfcf7bbeab49dd5c7bc2615673313687fc85c983
Daniel Hjelseth Høyer <mail@dahoiv.net>
2020-08-20 09:38:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     install_requires=['bluepy'],
     version = '0.8.0',
     description = 'A library to communicate with Switchbot',
-    author='Daniel Hoyer',
+    author='Daniel Hjelseth Hoyer',
     url='https://github.com/Danielhiversen/pySwitchbot/',
     classifiers=[
         'Development Status :: 3 - Alpha',
