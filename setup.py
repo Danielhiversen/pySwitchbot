1357c7c54403296172ad51fa3db2b3748b0e56f5
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-05-12 19:05:27 +0200

    0.10.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'PySwitchbot',
     packages = ['switchbot'],
     install_requires=['bluepy'],
-    version = '0.9.1',
+    version = '0.10.0',
     description = 'A library to communicate with Switchbot',
     author='Daniel Hjelseth Hoyer',
     url='https://github.com/Danielhiversen/pySwitchbot/',
