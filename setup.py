946dfbea4e3d5744e505010f76f8b2fb7f48a9ed
RenierM26 <66512715+RenierM26@users.noreply.github.com>
2021-06-26 13:03:15 +0200

    Start scan in separate thread (to not disconnect connected devices), fix mac address if entered in incorrect format.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'PySwitchbot',
     packages = ['switchbot'],
     install_requires=['bluepy'],
-    version = '0.11.0',
+    version = '0.11.1',
     description = 'A library to communicate with Switchbot',
     author='Daniel Hjelseth Hoyer',
     url='https://github.com/Danielhiversen/pySwitchbot/',
