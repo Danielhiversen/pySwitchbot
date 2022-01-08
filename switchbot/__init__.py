508322ab6f70c36415f18d7161570fde08ecb2c8
RenierM26 <66512715+RenierM26@users.noreply.github.com>
2022-01-08 11:07:32 +0200

    Update __init__.py

--- switchbot/__init__.py
+++ switchbot/__init__.py
@@ -40,7 +40,6 @@ KEY_PASSWORD_PREFIX = "571"
 _LOGGER = logging.getLogger(__name__)
 CONNECT_LOCK = Lock()
 
-#fix connectivity failure notifications
 def _sb_uuid(comms_type: str = "service") -> bluepy.btle.UUID:
     """Return Switchbot UUID."""
 
@@ -402,6 +401,9 @@ class SwitchbotDevice(bluepy.btle.Peripheral):
                 _LOGGER.warning("Error talking to Switchbot", exc_info=True)
             finally:
                 self.disconnect()
+
+        print(notify_msg)
+                
         if send_success:
             if notify_msg == b"\x07":
                 _LOGGER.error("Password required")
