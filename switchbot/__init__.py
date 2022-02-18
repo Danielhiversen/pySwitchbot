2b21753da00f6af03ddf22c47653610f4e9af1dd
Heisenberg <58039006+Heisenberg2980@users.noreply.github.com>
2022-02-18 16:27:50 +0000

    Update __init__.py
    Commenting line 283 out to avoid the issue where switchbot stops responding until HA restart. 
    If the switchbot is in disc state, it tries to read a response but it will never get one. By removing disc from the while loop, it will fall into BTLEDisconnectError exception and retry properly the next time around, without any deadlocks.

--- switchbot/__init__.py
+++ switchbot/__init__.py
@@ -280,7 +280,7 @@ class SwitchbotDevice(bluepy.btle.Peripheral):
         while rsp.get("state") and rsp["state"][0] in [
             "tryconn",
             "scan",
-            "disc",
+            #"disc",    
         ]:  # Wait for any operations to finish.
             rsp = self._getResp(["stat", "err"], timeout)
 
