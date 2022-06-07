361985df34c2f685326930d61c628095c0b94eaf
Ninad P.Chaudhari <ninadpchaudhari@gmail.com>
2022-06-07 15:31:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name = 'PySwitchbot',
-    packages = ['switchbot'],
+    name = 'PySwitchbot-read-patched',
+    packages = ['switchbot-read-patched'],
     install_requires=['bluepy'],
-    version = '0.13.3',
-    description = 'A library to communicate with Switchbot',
+    version = '0.13.4',
+    description = 'A Unofficial library to communicate with Switchbot',
     author='Daniel Hjelseth Hoyer',
-    url='https://github.com/Danielhiversen/pySwitchbot/',
+    url='https://github.com/Switchbot-Python/pySwitchbot-patched',
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
