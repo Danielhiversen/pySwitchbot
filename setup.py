7249dcd80eac0f8d707d03ee1ca6acce73c9ee11
J. Nick Koston <nick@koston.org>
2022-08-02 13:40:19 -1000

    0.17.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'PySwitchbot',
     packages = ['switchbot'],
     install_requires=['async_timeout>=4.0.1', 'bleak', 'bleak-retry-connector>=1.1.1'],
-    version = '0.17.1',
+    version = '0.17.2',
     description = 'A library to communicate with Switchbot',
     author='Daniel Hjelseth Hoyer',
     url='https://github.com/Danielhiversen/pySwitchbot/',
