31a2686133648e41025a28c4300ba0272db60c42
J. Nick Koston <nick@koston.org>
2022-08-04 14:45:41 -1000

    Additional refactoring to make it easier to add new devices (#62)

--- switchbot/const.py
+++ switchbot/const.py
@@ -4,3 +4,16 @@ from __future__ import annotations
 DEFAULT_RETRY_COUNT = 3
 DEFAULT_RETRY_TIMEOUT = 1
 DEFAULT_SCAN_TIMEOUT = 5
+
+from .enum import StrEnum
+
+
+class SwitchbotModel(StrEnum):
+
+    BOT = "WoHand"
+    CURTAIN = "WoCurtain"
+    PLUG_MINI = "WoPlug"
+    CONTACT_SENSOR = "WoContact"
+    METER = "WoSensorTH"
+    MOTION_SENSOR = "WoPresence"
+    COLOR_BULB = "WoBulb"
