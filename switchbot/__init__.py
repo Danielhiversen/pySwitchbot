20703cb9aa561ded0a148f8c2f35af9a0a77593b
J. Nick Koston <nick@koston.org>
2022-08-28 10:21:53 -0500

    cleanup

--- switchbot/__init__.py
+++ switchbot/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from .adv_parser import SwitchbotSupportedType, parse_advertisement_data
 from .const import SwitchbotModel
+from .devices.base_light import SwitchbotBaseLight
 from .devices.bot import Switchbot
 from .devices.bulb import SwitchbotBulb
 from .devices.curtain import SwitchbotCurtain
@@ -17,6 +18,7 @@ __all__ = [
     "GetSwitchbotDevices",
     "SwitchBotAdvertisement",
     "ColorMode",
+    "SwitchbotBaseLight",
     "SwitchbotBulb",
     "SwitchbotDevice",
     "SwitchbotCurtain",
