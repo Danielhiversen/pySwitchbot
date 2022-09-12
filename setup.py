067d1d072dac85fe95db204e8fb3577e56387f4d
J. Nick Koston <nick@koston.org>
2022-09-12 16:53:18 -0500

    bump bleak versions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="PySwitchbot",
     packages=["switchbot", "switchbot.devices", "switchbot.adv_parsers"],
-    install_requires=["async_timeout>=4.0.1", "bleak", "bleak-retry-connector>=1.14.0"],
+    install_requires=["async_timeout>=4.0.1", "bleak>=0.17.0", "bleak-retry-connector>=1.15.0"],
     version="0.19.6",
     description="A library to communicate with Switchbot",
     author="Daniel Hjelseth Hoyer",
