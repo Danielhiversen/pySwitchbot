b1cb0819d43e61f86d9562b795edfae12688522d
J. Nick Koston <nick@koston.org>
2022-12-16 17:53:25 -1000

    Fix temp sensors with passive

--- switchbot/adv_parsers/meter.py
+++ switchbot/adv_parsers/meter.py
@@ -6,19 +6,29 @@ from typing import Any
 
 def process_wosensorth(data: bytes | None, mfr_data: bytes | None) -> dict[str, Any]:
     """Process woSensorTH/Temp sensor services data."""
-    if data is None:
+    if mfr_data:
+        temp_data = mfr_data[8:11]
+        battery = None
+
+    if data:
+        temp_data = data[3:6]
+        battery = data[2] & 0b01111111
+
+    if not temp_data:
         return {}
 
-    _temp_sign = 1 if data[4] & 0b10000000 else -1
-    _temp_c = _temp_sign * ((data[4] & 0b01111111) + ((data[3] & 0b00001111) / 10))
+    _temp_sign = 1 if temp_data[1] & 0b10000000 else -1
+    _temp_c = _temp_sign * (
+        (temp_data[1] & 0b01111111) + ((temp_data[0] & 0b00001111) / 10)
+    )
     _temp_f = (_temp_c * 9 / 5) + 32
     _temp_f = (_temp_f * 10) / 10
 
     _wosensorth_data = {
         "temp": {"c": _temp_c, "f": _temp_f},
-        "fahrenheit": bool(data[5] & 0b10000000),
-        "humidity": data[5] & 0b01111111,
-        "battery": data[2] & 0b01111111,
+        "fahrenheit": bool(temp_data[2] & 0b10000000),
+        "humidity": temp_data[2] & 0b01111111,
+        "battery": battery,
     }
 
     return _wosensorth_data
