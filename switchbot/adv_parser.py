a2a68419c549f5a597a5952974c6fd438f012679
J. Nick Koston <nick@koston.org>
2022-12-16 21:46:48 -1000

    fix

--- switchbot/adv_parser.py
+++ switchbot/adv_parser.py
@@ -59,6 +59,7 @@ SUPPORTED_TYPES: dict[str, SwitchbotSupportedType] = {
         "modelName": SwitchbotModel.MOTION_SENSOR,
         "modelFriendlyName": "Motion Sensor",
         "func": process_wopresence,
+        "manufacturer_data_length": 10,
     },
     "r": {
         "modelName": SwitchbotModel.LIGHT_STRIP,
