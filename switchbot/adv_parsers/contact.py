9bad836202981db4ad6129b85b398a88900caee3
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2023-01-13 07:17:27 +1100

    Fix contact_open timeout (#184)
    Co-authored-by: J. Nick Koston <nick@koston.org>

--- switchbot/adv_parsers/contact.py
+++ switchbot/adv_parsers/contact.py
@@ -11,16 +11,17 @@ def process_wocontact(
 
     battery = data[2] & 0b01111111 if data else None
     tested = bool(data[1] & 0b10000000) if data else None
-    contact_timeout = data[3] & 0b00000100 == 0b00000100 if data else False
 
     if mfr_data and len(mfr_data) >= 13:
         motion_detected = bool(mfr_data[7] & 0b10000000)
         contact_open = bool(mfr_data[7] & 0b00010000)
+        contact_timeout = bool(mfr_data[7] & 0b00100000)
         button_count = mfr_data[12] & 0b00001111
         is_light = bool(mfr_data[7] & 0b01000000)
     else:
         motion_detected = bool(data[1] & 0b01000000)
-        contact_open = data[3] & 0b00000010 == 0b00000010
+        contact_open = bool(data[3] & 0b00000010)
+        contact_timeout = bool(data[3] & 0b00000100)
         button_count = data[8] & 0b00001111
         is_light = bool(data[3] & 0b00000001)
 
