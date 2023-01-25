afd26a3b18b22b08fc0b41bc215800f7612bb66c
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2023-01-25 14:44:59 +1300

    Remove constructor Rename variable

--- switchbot/devices/blind_tilt.py
+++ switchbot/devices/blind_tilt.py
@@ -12,17 +12,12 @@ _LOGGER = logging.getLogger(__name__)
 class SwitchbotBlindTilt(SwitchbotCurtain):
     """Representation of a Switchbot Blind Tilt."""
 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        """Switchbot Blind Tilt/woBlindTilt constructor."""
-
-        super().__init__(*args, **kwargs)
-
     async def get_basic_info(self) -> dict[str, Any] | None:
         """Get device basic settings."""
         if not (_data := await self._get_basic_info()):
             return None
 
-        _position = max(min(_data[6], 100), 0)
+        _tilt = max(min(_data[6], 100), 0)
         return {
             "battery": _data[1],
             "firmware": _data[2] / 10.0,
@@ -36,7 +31,7 @@ class SwitchbotBlindTilt(SwitchbotCurtain):
                 "up": bool(_data[5] & (0b00000010 if self._reverse else 0b00000001)),
                 "down": bool(_data[5] & (0b00000001 if self._reverse else 0b00000010)),
             },
-            "position": (100 - _position) if self._reverse else _position,
+            "tilt": (100 - _tilt) if self._reverse else _tilt,
             "timers": _data[7],
         }
 
