f7cc161c7152de2ebf8db9e43914ba5d7fd172e5
J. Nick Koston <nick@koston.org>
2023-03-22 14:27:51 -1000

    0.37.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "boto3>=1.20.24",
         "requests>=2.28.1",
     ],
-    version="0.37.3",
+    version="0.37.4",
     description="A library to communicate with Switchbot",
     author="Daniel Hjelseth Hoyer",
     url="https://github.com/Danielhiversen/pySwitchbot/",
