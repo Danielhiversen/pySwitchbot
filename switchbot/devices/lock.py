7c7e7d05d0d1646d87aff875e2562b18fa04b840
huyuwei1996 <huyuwei1996@gmail.com>
2024-05-22 22:16:12 +0800

    feat: support users from different regions in obtaining lock encryption key.

--- switchbot/devices/lock.py
+++ switchbot/devices/lock.py
@@ -1,20 +1,17 @@
 """Library to handle connection with Switchbot Lock."""
+
 from __future__ import annotations
 
-import base64
-import hashlib
-import hmac
 import json
 import logging
 import time
 from typing import Any
 
-import boto3
 import requests
 from bleak.backends.device import BLEDevice
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
-from ..api_config import SWITCHBOT_APP_API_BASE_URL, SWITCHBOT_APP_COGNITO_POOL
+from ..api_config import SWITCHBOT_ACCOUNT_API_BASE_URL, SWITCHBOT_APP_API_BASE_URL
 from ..const import (
     LockStatus,
     SwitchbotAccountConnectionError,
@@ -87,75 +84,74 @@ class SwitchbotLock(SwitchbotDevice):
         return lock_info is not None
 
     @staticmethod
-    def retrieve_encryption_key(device_mac: str, username: str, password: str):
-        """Retrieve lock key from internal SwitchBot API."""
-        device_mac = device_mac.replace(":", "").replace("-", "").upper()
-        msg = bytes(username + SWITCHBOT_APP_COGNITO_POOL["AppClientId"], "utf-8")
-        secret_hash = base64.b64encode(
-            hmac.new(
-                SWITCHBOT_APP_COGNITO_POOL["AppClientSecret"].encode(),
-                msg,
-                digestmod=hashlib.sha256,
-            ).digest()
-        ).decode()
-
-        cognito_idp_client = boto3.client(
-            "cognito-idp", region_name=SWITCHBOT_APP_COGNITO_POOL["Region"]
-        )
+    def post(
+        log_name: str, url: str, body: dict, access_token: str | None = None
+    ) -> dict:
         try:
-            auth_response = cognito_idp_client.initiate_auth(
-                ClientId=SWITCHBOT_APP_COGNITO_POOL["AppClientId"],
-                AuthFlow="USER_PASSWORD_AUTH",
-                AuthParameters={
-                    "USERNAME": username,
-                    "PASSWORD": password,
-                    "SECRET_HASH": secret_hash,
-                },
-            )
-        except cognito_idp_client.exceptions.NotAuthorizedException as err:
-            raise SwitchbotAuthenticationError(
-                f"Failed to authenticate: {err}"
-            ) from err
-        except Exception as err:
-            raise SwitchbotAuthenticationError(
-                f"Unexpected error during authentication: {err}"
-            ) from err
-
-        if (
-            auth_response is None
-            or "AuthenticationResult" not in auth_response
-            or "AccessToken" not in auth_response["AuthenticationResult"]
-        ):
-            raise SwitchbotAuthenticationError("Unexpected authentication response")
-
-        access_token = auth_response["AuthenticationResult"]["AccessToken"]
-        try:
-            key_response = requests.post(
-                url=SWITCHBOT_APP_API_BASE_URL + "/developStage/keys/v1/communicate",
-                headers={"authorization": access_token},
-                json={
-                    "device_mac": device_mac,
-                    "keyType": "user",
-                },
+            response = requests.post(
+                url=url,
+                headers={"authorization": access_token} if access_token else None,
+                json=body,
                 timeout=10,
             )
         except requests.exceptions.RequestException as err:
             raise SwitchbotAccountConnectionError(
-                f"Failed to retrieve encryption key from SwitchBot Account: {err}"
+                f"Failed to retrieve {log_name} from SwitchBot Account: {err}"
             ) from err
-        if key_response.status_code > 299:
+        if response.status_code > 299:
             raise SwitchbotAuthenticationError(
-                f"Unexpected status code returned by SwitchBot Account API: {key_response.status_code}"
+                f"Unexpected status code returned by SwitchBot Account API: {response.status_code}"
             )
-        key_response_content = json.loads(key_response.content)
-        if key_response_content["statusCode"] != 100:
+        resp = response.json()
+        if resp["statusCode"] != 100:
             raise SwitchbotAuthenticationError(
-                f"Unexpected status code returned by SwitchBot API: {key_response_content['statusCode']}"
+                f"Unexpected status code returned by SwitchBot API: {resp['statusCode']}"
             )
+        return resp
+
+    @staticmethod
+    def retrieve_encryption_key(device_mac: str, username: str, password: str):
+        """Retrieve lock key from internal SwitchBot API."""
+        device_mac = device_mac.replace(":", "").replace("-", "").upper()
+
+        auth_response = SwitchbotLock.post(
+            log_name="login credentials",
+            url=SWITCHBOT_ACCOUNT_API_BASE_URL + "/account/api/v1/user/login",
+            body={
+                "clientId": "5nnwmhmsa9xxskm14hd85lm9bm",
+                "grantType": "password",
+                "password": password,
+                "username": username,
+                "verifyCode": "",
+            },
+        )
+        access_token = auth_response["body"]["access_token"]
+
+        user_response = SwitchbotLock.post(
+            log_name="user info",
+            url=SWITCHBOT_ACCOUNT_API_BASE_URL + "/account/api/v1/user/userinfo",
+            body={},
+            access_token=access_token,
+        )
+        user_region = (
+            user_response["body"]["botRegion"]
+            if user_response["body"]["botRegion"]
+            else "us"
+        )
+
+        key_response = SwitchbotLock.post(
+            log_name="encryption key",
+            url=SWITCHBOT_APP_API_BASE_URL.format(user_region) + "/keys/v1/communicate",
+            body={
+                "device_mac": device_mac,
+                "keyType": "user",
+            },
+            access_token=access_token,
+        )
 
         return {
-            "key_id": key_response_content["body"]["communicationKey"]["keyId"],
-            "encryption_key": key_response_content["body"]["communicationKey"]["key"],
+            "key_id": key_response["body"]["communicationKey"]["keyId"],
+            "encryption_key": key_response["body"]["communicationKey"]["key"],
         }
 
     async def lock(self) -> bool:
