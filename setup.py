29216b5be861523619576be06ec0019c17d303b8
J. Nick Koston <nick@koston.org>
2024-07-10 23:25:09 -0500

    0.48.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "cryptography>=39.0.0",
         "pyOpenSSL>=23.0.0",
     ],
-    version="0.48.0",
+    version="0.48.1",
     description="A library to communicate with Switchbot",
     author="Daniel Hjelseth Hoyer",
     url="https://github.com/Danielhiversen/pySwitchbot/",
