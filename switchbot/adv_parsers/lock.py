3f5a64241d0000e2a9ac0886acac06ac6a65c98b
Leo Shen <i@szclsya.me>
2024-07-10 23:37:08 -0400

    adv_parsers/lock: remove manual field for now
    - as it's not needed by HA for now and is actually inaccurate

--- switchbot/adv_parsers/lock.py
+++ switchbot/adv_parsers/lock.py
@@ -50,7 +50,6 @@ def process_wolock_pro(
         "unlocked_alarm": bool(mfr_data[11] & 0b01000000),
         "auto_lock_paused": bool(mfr_data[8] & 0b100000),
         "night_latch": bool(mfr_data[9] & 0b00000001),
-        "manual": not bool(mfr_data[7] & 0b00000010),
     }
     _LOGGER.debug(res)
     return res
