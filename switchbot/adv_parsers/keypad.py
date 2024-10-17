536f7c5fe56e244a1f27d1520cc0de73e3189328
Nick Conway <nick@conway.dev>
2024-10-17 16:22:44 -0400

    feat(keypad-touch): change success sensor to binary sensor

--- switchbot/adv_parsers/keypad.py
+++ switchbot/adv_parsers/keypad.py
@@ -6,6 +6,8 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
+lastStatus = -1
+
 
 def process_wokeypad(
     data: bytes | None, mfr_data: bytes | None
@@ -18,4 +20,11 @@ def process_wokeypad(
     if data:
         _LOGGER.debug("data: %s", data.hex())
 
-    return {"battery": data[2], "attemptState": mfr_data[6]}
+    global lastStatus
+    success = lastStatus != -1 and (
+        (mfr_data[6] > lastStatus and mfr_data[6] - lastStatus >= 2)
+        or (mfr_data[6] < lastStatus and mfr_data[6] - lastStatus >= -254)
+    )
+    lastStatus = mfr_data[6]
+
+    return {"battery": data[2], "success": success}
