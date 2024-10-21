04a0507bb3b828bb70afc1a2309fc259eb294a19
Nick Conway <nick@conway.dev>
2024-10-21 19:15:35 -0400

    feat(keypad-touch): remove global variables

--- switchbot/adv_parsers/keypad.py
+++ switchbot/adv_parsers/keypad.py
@@ -6,25 +6,27 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-lastStatus = -1
-
 
 def process_wokeypad(
     data: bytes | None, mfr_data: bytes | None
 ) -> dict[str, bool | int | None]:
     """Process woKeypad services data."""
     if data is None or mfr_data is None:
+        process_wokeypad.lastStatus = -1
         return {"battery": None, "attemptState": None}
 
     _LOGGER.debug("mfr_data: %s", mfr_data.hex())
     if data:
         _LOGGER.debug("data: %s", data.hex())
 
-    global lastStatus
+    lastStatus = process_wokeypad.lastStatus
     success = lastStatus != -1 and (
         (mfr_data[6] > lastStatus and mfr_data[6] - lastStatus >= 2)
         or (mfr_data[6] < lastStatus and mfr_data[6] - lastStatus >= -254)
     )
-    lastStatus = mfr_data[6]
+    process_wokeypad.lastStatus = mfr_data[6]
 
     return {"battery": data[2], "success": success}
+
+
+process_wokeypad.lastStatus = -1
