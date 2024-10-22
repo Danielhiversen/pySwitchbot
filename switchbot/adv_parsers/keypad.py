643e7a8aae78e462473ff341e68a46c1b284ef25
Nick Conway <nick@conway.dev>
2024-10-22 17:17:07 -0400

    feat(keypad-touch): create device class to support multiple keypads

--- switchbot/adv_parsers/keypad.py
+++ switchbot/adv_parsers/keypad.py
@@ -8,25 +8,21 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def process_wokeypad(
-    data: bytes | None, mfr_data: bytes | None
+    data: bytes | None, mfr_data: bytes | None, lastStatus: list[int] = [-1]
 ) -> dict[str, bool | int | None]:
     """Process woKeypad services data."""
     if data is None or mfr_data is None:
-        process_wokeypad.lastStatus = -1
+        lastStatus[0] = -1
         return {"battery": None, "attemptState": None}
 
     _LOGGER.debug("mfr_data: %s", mfr_data.hex())
     if data:
         _LOGGER.debug("data: %s", data.hex())
 
-    lastStatus = process_wokeypad.lastStatus
-    success = lastStatus != -1 and (
-        (mfr_data[6] > lastStatus and mfr_data[6] - lastStatus >= 2)
-        or (mfr_data[6] < lastStatus and mfr_data[6] - lastStatus >= -254)
+    success = lastStatus[0] != -1 and (
+        (mfr_data[6] > lastStatus[0] and mfr_data[6] - lastStatus[0] >= 2)
+        or (mfr_data[6] < lastStatus[0] and mfr_data[6] - lastStatus[0] >= -254)
     )
-    process_wokeypad.lastStatus = mfr_data[6]
+    lastStatus[0] = mfr_data[6]
 
     return {"battery": data[2], "success": success}
-
-
-process_wokeypad.lastStatus = -1
--- switchbot/devices/keypad.py
+++ switchbot/devices/keypad.py
@@ -1,1 +1,17 @@
 from __future__ import annotations
+
+import logging
+from typing import Any
+
+from .device import SwitchbotDevice
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SwitchbotKeypad(SwitchbotDevice):
+    """Representation of a Switchbot keypad."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Switchbot keypad constructor."""
+        self._lastStatus: list[int] = [-1]
+        super().__init__(self._lastStatus, *args, **kwargs)
